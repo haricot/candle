ae922817fe180d5f8a42544f1e507450e1af274e
Nicolas PASCAL <344493+haricot@users.noreply.github.com>
2026-08-25 22:04:19 +0200

    fix(cuda): balance BF16 and FP8 binary guards

--- candle-kernels/src/binary.cu
+++ candle-kernels/src/binary.cu
@@ -16,8 +16,6 @@ BINARY_OP_OUT(__nv_bfloat16, uint8_t, gt_bf16, x > y)
 BINARY_OP_OUT(__nv_bfloat16, uint8_t, ge_bf16, x >= y)
 #endif
 
-#endif
-
 #if __CUDA_ARCH__ >= 800 || defined(CANDLE_CUDA_LEGACY_FP8)
 #define F8E4M3_TO_FLOAT(x) __half2float(__nv_cvt_fp8_to_halfraw(x.__x, __NV_E4M3))
 
