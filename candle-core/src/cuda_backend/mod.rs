b01acbe1b3eaabd7f5ffc2054121413f402acfc4
Nicolas PASCAL <344493+haricot@users.noreply.github.com>
2026-08-26 10:08:02 +0200

    fix(cuda): use contiguous conv2d kernel storage

--- candle-core/src/cuda_backend/mod.rs
+++ candle-core/src/cuda_backend/mod.rs
@@ -1587,9 +1587,8 @@ impl CudaStorage {
                     .alloc_uninit(kernel_l.shape(), kernel.dtype())?
             };
             kernel.copy_strided_src(&mut kernel_c, 0, kernel_l)?;
-            let kernel_l =
-                Layout::contiguous_with_offset((n, k), kernel_l.start_offset()).transpose(0, 1)?;
-            col.matmul(kernel, (1, b * m, n, k), &col_l, &kernel_l)?
+            let kernel_l = Layout::contiguous((n, k)).transpose(0, 1)?;
+            col.matmul(&kernel_c, (1, b * m, n, k), &col_l, &kernel_l)?
         };
         let res_l = Layout::contiguous((b, h_out, w_out, n))
             .transpose(1, 2)?
