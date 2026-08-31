b90896f459466364375c624414770fa70a7a97ff
Nicolas PASCAL <344493+haricot@users.noreply.github.com>
2026-08-31 06:01:04 +0200

    fix(cuda): restore half atomic add on pre-Volta GPUs

--- candle-kernels/src/compatibility.cuh
+++ candle-kernels/src/compatibility.cuh
@@ -62,6 +62,46 @@ __device__ double atomicAdd(double* address, double val) {
 //}
 #endif
 
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 700
+__device__ __forceinline__ __half atomicAdd(__half* address, __half val) {
+    unsigned int* address_as_ui =
+        (unsigned int*)((char*)address - ((size_t)address & 2));
+    unsigned int old = *address_as_ui;
+    unsigned int assumed;
+    const bool upper = ((size_t)address & 2) != 0;
+
+    do {
+        assumed = old;
+
+        const unsigned short old_bits =
+            upper ? (unsigned short)(old >> 16)
+                  : (unsigned short)(old & 0xffff);
+
+        const __half current = __ushort_as_half(old_bits);
+
+        const __half sum = __float2half(
+            __half2float(current) + __half2float(val)
+        );
+
+        const unsigned int sum_bits =
+            (unsigned int)__half_as_ushort(sum);
+
+        const unsigned int updated =
+            upper
+                ? ((old & 0x0000ffffu) | (sum_bits << 16))
+                : ((old & 0xffff0000u) | sum_bits);
+
+        old = atomicCAS(address_as_ui, assumed, updated);
+    } while (old != assumed);
+
+    const unsigned short result_bits =
+        upper ? (unsigned short)(old >> 16)
+              : (unsigned short)(old & 0xffff);
+
+    return __ushort_as_half(result_bits);
+}
+#endif
+
 
 __device__ __forceinline__ __half atomicMaxf(__half* address, __half val) {
 #if __CUDA_ARCH__ < 700
